91978aa4ae61a35756d2692969077dd1e19918ff
bqubique <blinnqipa@gmail.com>
2022-09-29 13:58:12 +0200

    Code clean-up. Add new method to take pictures.

--- lib/camera_feature_controller.dart
+++ lib/camera_feature_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:riverpod/riverpod.dart';
 import 'package:camera/camera.dart';
@@ -11,7 +12,14 @@ final applicationCameraControllerProvider =
     StateNotifierProvider<CameraFeatureController, CameraState>((ref) {
   return CameraFeatureController(
     CameraState(
-      controller: CameraController(cameras[0], ResolutionPreset.max),
+      controller: CameraController(
+        cameras[0],
+        ResolutionPreset.max,
+        enableAudio: true,
+        imageFormatGroup: Platform.isAndroid
+            ? ImageFormatGroup.yuv420
+            : ImageFormatGroup.bgra8888,
+      ),
       lastPictureTaken: XFile(''),
     ),
   );
@@ -25,9 +33,9 @@ class CameraFeatureController extends StateNotifier<CameraState> {
   ///This method will select a new camera and set the controller to that camera
   ///will check whether the old controller is null or not and dispose that controller
   ///respectively.
-  ///
-  ///Call this when you want to switch from front to rear camera or vice versa.
-  void onNewCameraSelected(CameraDescription cameraDescription) async {
+  ///Call this when you want to switch from front to rear camera, or when you're
+  ///selecting a camera for the first time.
+  Future<void> onNewCameraSelected(CameraDescription cameraDescription) async {
     //Get the old controller, set it to null and dispose it
     final CameraController? oldController = state.controller;
     if (oldController != null) {
@@ -39,7 +47,9 @@ class CameraFeatureController extends StateNotifier<CameraState> {
       cameraDescription,
       ResolutionPreset.max,
       enableAudio: true,
-      imageFormatGroup: ImageFormatGroup.jpeg,
+      imageFormatGroup: Platform.isAndroid
+          ? ImageFormatGroup.yuv420
+          : ImageFormatGroup.bgra8888,
     );
 
     state = state.copyWith(controller: cameraController);
@@ -75,55 +85,58 @@ class CameraFeatureController extends StateNotifier<CameraState> {
   ///Checks if the current state's camera is back or front, reverts it by
   ///iterating through the list of cameras retrieved in the [main] function and
   ///updates the state's controller through [onNewCameraSelected] method of state controller
-  void switchCamera() {
+  Future<void> switchCamera() async {
     if (state.controller!.description.lensDirection ==
         CameraLensDirection.back) {
-      for (final CameraDescription cameraDescription in cameras) {
-        if (cameraDescription.lensDirection == CameraLensDirection.front) {
-          onNewCameraSelected(cameraDescription);
-        }
-      }
+      await onNewCameraSelected(cameras.firstWhere((selectedCamera) =>
+          selectedCamera.lensDirection == CameraLensDirection.front));
     } else {
-      for (final CameraDescription cameraDescription in cameras) {
-        if (cameraDescription.lensDirection == CameraLensDirection.back) {
-          onNewCameraSelected(cameraDescription);
-        }
-      }
+      await onNewCameraSelected(cameras.firstWhere((selectedCamera) =>
+          selectedCamera.lensDirection == CameraLensDirection.back));
     }
   }
 
   ///Switches the camera to [CameraLensDirection.front] if current LensDirection is [CameraLensDirection.back]
-  void switchToFrontCamera() {
-    state.controller!.addListener(() {
-      if (state.controller!.value.isInitialized) {
-        if (state.controller!.value.isInitialized) {
-          if (state.controller!.description.lensDirection ==
-              CameraLensDirection.back) {
-            for (final CameraDescription cameraDescription in cameras) {
-              if (cameraDescription.lensDirection ==
-                  CameraLensDirection.front) {
-                onNewCameraSelected(cameraDescription);
-              }
-            }
-          }
-        }
-      }
-    });
+  Future<void> switchToFrontCamera() async {
+    if (state.controller!.description.lensDirection ==
+        CameraLensDirection.front) {
+      return;
+    }
+
+    await onNewCameraSelected(cameras.firstWhere((selectedCamera) =>
+        selectedCamera.lensDirection == CameraLensDirection.front));
   }
 
   ///Switches the camera to [CameraLensDirection.back] if current LensDirection is [CameraLensDirection.front]
-  void switchToRearCamera() {
-    state.controller!.addListener(() {
-      if (state.controller!.value.isInitialized) {
-        if (state.controller!.description.lensDirection ==
-            CameraLensDirection.front) {
-          for (final CameraDescription cameraDescription in cameras) {
-            if (cameraDescription.lensDirection == CameraLensDirection.back) {
-              onNewCameraSelected(cameraDescription);
-            }
-          }
-        }
-      }
-    });
+  Future<void> switchToRearCamera() async {
+    if (state.controller!.description.lensDirection ==
+        CameraLensDirection.back) {
+      return;
+    }
+
+    await onNewCameraSelected(cameras.firstWhere((selectedCamera) =>
+        selectedCamera.lensDirection == CameraLensDirection.back));
+  }
+
+  ///Safely take a picture using the camera controller by checking for [controller.isTakingPicture] property before.
+  Future<XFile?> takePicture() async {
+    final CameraController? cameraController = state.controller;
+    if (cameraController == null || !cameraController.value.isInitialized) {
+      log('Error: select a camera first.');
+      return null;
+    }
+
+    if (cameraController.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+
+    try {
+      final XFile file = await cameraController.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      log(e.description!);
+      return null;
+    }
   }
 }
